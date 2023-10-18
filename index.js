03877f4e27ae0534aadf38977b82c2c61aff2961
rume edgar <rumeedgar@icloud.com>
2023-10-18 17:29:59 +0100

    formatting coin data

--- index.js
+++ index.js
@@ -15,17 +15,38 @@ async function getPriceFeed() {
     const elmSelector =
       "#__next > div.sc-faa5ca00-1.cKgcaj.global-layout-v2 > div.main-content > div.cmc-body-wrapper > div > div:nth-child(1) > div.sc-66133f36-2.cgmess > table > tbody > tr";
 
+    const keys = [
+      "rank",
+      "name",
+      "price",
+      "1h",
+      "24h",
+      "7d",
+      "marketCap",
+      "volume",
+      "circulatingSupply",
+    ];
+
     $(elmSelector).each((parentIdx, parentElem) => {
+      let keyIdx = 0;
+      const coinObj = {};
+
       if (parentIdx <= 9) {
         $(parentElem)
           .children()
           .each((childIdx, childElem) => {
-            const tdValue = $(childElem).text();
+            let tdValue = $(childElem).text();
+
+            if (keyIdx === 1 || keyIdx === 7) {
+              console.log($("p:first-child", $(childElem).html()).text());
+            }
 
             if (tdValue) {
-              console.log(tdValue);
+              coinObj[keys[keyIdx]] = tdValue;
+              keyIdx++;
             }
           });
+        // console.log(coinObj);
       }
     });
   } catch (err) {
