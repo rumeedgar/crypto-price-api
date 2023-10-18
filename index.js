c56c330e531f30a0d87f673e8f087981f54e42eb
rume edgar <rumeedgar@icloud.com>
2023-10-18 16:42:45 +0100

    update

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-const axios = require('axios')
-const cheerio = require('cheerio')
-const express = require('express')
+const axios = require("axios");
+const cheerio = require("cheerio");
+const express = require("express");
+
+async function getPriceFeed() {
+  try {
+    const siteUrl = "https://coinmarketcap.com/";
+
+    const { data } = await axios({
+      method: "GET",
+      url: siteUrl,
+    });
+
+    const $ = cheerio.load(data);
+    const elmSelector =
+      "#__next > div.sc-faa5ca00-1.cKgcaj.global-layout-v2 > div.main-content > div.cmc-body-wrapper > div > div:nth-child(1) > div.sc-66133f36-2.cgmess > table > tbody > tr";
+
+    $(elmSelector).each((parentIdx, parentElem) => {
+      console.log(parentElem);
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+getPriceFeed();
