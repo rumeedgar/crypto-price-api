d19cf7a7246e4b6fd581b7d101a8b66c841e4744
rume edgar <rumeedgar@icloud.com>
2023-10-18 16:56:56 +0100

    scraping the coin data from the website

--- index.js
+++ index.js
@@ -16,7 +16,17 @@ async function getPriceFeed() {
       "#__next > div.sc-faa5ca00-1.cKgcaj.global-layout-v2 > div.main-content > div.cmc-body-wrapper > div > div:nth-child(1) > div.sc-66133f36-2.cgmess > table > tbody > tr";
 
     $(elmSelector).each((parentIdx, parentElem) => {
-      console.log(parentElem);
+      if (parentIdx <= 9) {
+        $(parentElem)
+          .children()
+          .each((childIdx, childElem) => {
+            const tdValue = $(childElem).text();
+
+            if (tdValue) {
+              console.log(tdValue);
+            }
+          });
+      }
     });
   } catch (err) {
     console.error(err);
